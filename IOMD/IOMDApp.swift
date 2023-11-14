c7dc4d4d55ed525b9b566aaa4400248aba7ae65a
AkhterI22202 <akhterhussain.dar@ideas2it.com>
2023-11-14 17:43:43 +0530

    first commit

---
+++ IOMD/IOMDApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOMDApp.swift
+//  IOMD
+//
+//  Created by Ideas2IT on 05/11/23.
+//
+
+import SwiftUI
+
+@main
+struct IOMDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
